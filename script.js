a5464e7be60cea46b13c5e7006f6ea8652c6a2ed
Brent Steffen <BESteffen82@gmail.com>
2022-05-02 15:33:20 -0400

    Add working operator functions to JS

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function neg(a){
 
 function operate (op,a,b){
     if (op === '+'){
-      return add(a,b);
+       return add(a,b);      
     }
     else if (op === '-'){
       return subtract(a,b);
@@ -38,27 +38,94 @@ function operate (op,a,b){
     else if (op === '^'){
       return power(a,b);
     }
-    else if (op === '+/-'){
-      return neg(a);
-    }    
 }
 
-let display = document.querySelector('.display');
-let numbers = document.querySelectorAll('.number-button');
-const numberDisplay = '';
-let numberValue;
-let counter = 0;
+const clear = document.getElementById('clear-button');
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.number-button');
+const operators = document.querySelectorAll('.operator-button');
+const equals = document.querySelector('.equal-button');
+let numberDisplay = '';
+let currentNumOne = '';
+let currentNumTwo = '';
+let a;
+let b;
 
+function firstNumBtn(){
+  numbers.forEach((button) => {
+    button.onclick = () => {
+      currentNumOne += numberDisplay.concat(button.value);
+      display.innerText = currentNumOne;
+      a = parseInt(currentNumOne);
+      console.log(a);                         
+    };
+  });
+};
 
+function secondNumBtn(){
 numbers.forEach((button) => {
-  button.onclick = () => {
-    numberValue = numberDisplay.concat(button.value);
-    display.textContent += numberValue;
-      };
-    });
+    button.onclick = () => {
+      currentNumTwo += numberDisplay.concat(button.value);
+      display.innerText = currentNumTwo;
+      b = parseInt(currentNumTwo);
+      console.log(b);                   
+    };
+  });
+};
+
+function firstOp (){
+  firstNumBtn();
+  operators.forEach((button) => {
+    button.onclick = () => {
+      if (button.id == 'plus'){
+        display.innerText = '';
+        secondNumBtn();                                                             
+      } else if (button.id == 'minus'){
+        display.innerText = '';
+        secondNumBtn();
+      } else if (button.id == 'multiple'){
+        display.innerText = '';
+        secondNumBtn();
+      } else if (button.id == 'divide'){
+        display.innerText = '';
+        secondNumBtn();
+      } else if (button.id == 'power'){
+        display.innerText = '';
+        secondNumBtn();       
+      };                   
+    };
+  });
+}
+
+function calculate(){
+  firstOp();
+  equals.onclick = () => {
+  display.innerText = '';
+  let c = operate('+', a, b);
+  display.innerText = parseInt(c);
+  };
+}
+
+calculate();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
     
 
-   
+
 
 
 
